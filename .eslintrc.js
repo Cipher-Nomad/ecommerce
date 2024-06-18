a13365b7e58908b06025f73ce0632011ff7d2e2b
Akash S G (Cipher-Nomad) <akashsg1398@gmail.com>
2024-06-18 16:31:23 +0530

    implement small fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,19 @@
-module.exports = {
-  root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
-}
+module.exports =
+  // root: true,
+  // extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  // ignorePatterns: ['**/payload-types.ts'],
+  // plugins: ['prettier'],
+
+  {
+    extends: ['airbnb', 'prettier'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+      'import/extensions': 0,
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'func-names': 'off',
+      'object-shorthand': 'off',
+      indent: ['error', 2],
+    },
+  }
